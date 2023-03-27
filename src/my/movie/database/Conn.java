5070cb0e3c80696b96e64a720f43932c5a7e556e
Atharv Dange <103875845+atharvdange618@users.noreply.github.com>
2023-03-27 21:54:23 +0530

    movie table updated
    added search, reset and back button, and implemented JTable

--- src/my/movie/database/Conn.java
+++ src/my/movie/database/Conn.java
@@ -15,5 +15,9 @@ public class Conn {
             System.out.println(e);
         }
     }
+
+    Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
 
--- src/my/movie/database/MovieTable.java
+++ src/my/movie/database/MovieTable.java
@@ -1,58 +1,113 @@
 package my.movie.database;
 
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import java.awt.event.*;
+import java.sql.*;
+import javax.swing.table.DefaultTableModel;
 
 public class MovieTable extends JFrame {
 
-    JTextArea textArea;
+    private JButton backButton;
+    private JButton searchButton;
+    private JButton resetButton;
+    private JTextField searchField;
+    private JTable table;
+    private Object[][] data;
+    private Object[][] originalData;
+    private Object[] columnNames;
 
-    MovieTable() {
+    public MovieTable() {
         setTitle("Movie Table");
-
-        // Create a JTextArea to display the movie names and genres
-        textArea = new JTextArea();
-        textArea.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-
-        // Add the scroll pane to the frame
-        add(scrollPane, BorderLayout.CENTER);
-
-        // Add a back button to the frame
-        JButton backButton = new JButton("Back");
-        backButton.setBackground(Color.white);
-        backButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Hide the current frame
-                setVisible(false);
-
-                // Create a new instance of the main page and show it
-                new MainPage().setVisible(true);
-            }
-        });
-        add(backButton, BorderLayout.SOUTH);
-
-        setSize(600, 400);
-        setVisible(true);
-        setLocation(350, 200);
 
         try {
             Conn c1 = new Conn();
             ResultSet rs = c1.s.executeQuery("SELECT moviename, genre FROM movies");
 
+            data = new Object[500][2];
+            originalData = new Object[500][2];
+            int i = 0;
             while (rs.next()) {
                 String name = rs.getString("moviename");
                 String genre = rs.getString("genre");
-                textArea.append(String.format("%-60s %s\n", name, genre));
+                data[i][0] = name;
+                data[i][1] = genre;
+                originalData[i][0] = name;
+                originalData[i][1] = genre;
+                i++;
             }
+
+            columnNames = new Object[]{"Movies/Series", "Genre"};
+
+            table = new JTable(data, columnNames);
+
+            JScrollPane scrollPane = new JScrollPane(table);
+
+            backButton = new JButton("Back");
+            backButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    dispose(); // close the current frame
+                    new MainPage(); // open the main page
+                }
+            });
+
+            searchButton = new JButton("Search");
+            searchButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    String searchTerm = searchField.getText().trim().toLowerCase();
+
+                    if (searchTerm.isEmpty()) {
+                        table.setModel(new DefaultTableModel(originalData, columnNames));
+                        data = originalData;
+                    } else {
+                        Object[][] searchData = new Object[500][2];
+                        int j = 0;
+                        for (int i = 0; i < originalData.length; i++) {
+                            String name = originalData[i][0] != null ? originalData[i][0].toString().toLowerCase() : "";
+                            if (name.contains(searchTerm)) {
+                                searchData[j][0] = originalData[i][0];
+                                searchData[j][1] = originalData[i][1];
+                                j++;
+                            }
+                        }
+                        searchData = resizeArray(searchData, j, 2);
+                        table.setModel(new DefaultTableModel(searchData, columnNames));
+                        data = searchData;
+                    }
+                }
+            });
+
+            resetButton = new JButton("Reset");
+            resetButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    table.setModel(new DefaultTableModel(originalData, columnNames));
+                    data = originalData;
+                    searchField.setText("");
+                }
+            });
+
+            searchField = new JTextField(20);
+            searchField.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    searchButton.doClick();
+                }
+            });
+
+            JPanel buttonPanel = new JPanel();
+            buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+            buttonPanel.add(resetButton);
+            buttonPanel.add(Box.createHorizontalGlue());
+            buttonPanel.add(new JLabel("Search:"));
+            buttonPanel.add(searchField);
+            buttonPanel.add(Box.createHorizontalStrut(10));
+            buttonPanel.add(backButton);
+
+            add(scrollPane, BorderLayout.CENTER);
+            add(buttonPanel, BorderLayout.NORTH);
+
+            setSize(800, 480);
+            setVisible(true);
+            setLocation(350, 200);
         } catch (SQLException e) {
             System.out.println(e);
         }
@@ -60,5 +115,13 @@ public class MovieTable extends JFrame {
 
     public static void main(String[] args) {
         new MovieTable();
+    }
+
+    private Object[][] resizeArray(Object[][] arr, int rows, int cols) {
+        Object[][] temp = new Object[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            System.arraycopy(arr[i], 0, temp[i], 0, cols);
+        }
+        return temp;
     }
 }
